0274d7a24a81225c9267485db17d9de3a1f4718e
Mowmo <md.dby.pro@gmail.com>
2024-03-29 09:08:31 +0100

    _blank button

--- src/pages/achievement.jsx
+++ src/pages/achievement.jsx
@@ -11,7 +11,7 @@ const Achievement = () => {
                 <div className="card-body text-center">
                   <h5 className="card-title">Portfolio</h5>
                   <p className="card-text">Site portfolio élégant, conçu avec Bootstrap et HTML pour mettre en valeur les projets et talents de notre client de manière professionnelle et réactive.</p>
-                  <a href="https://github.com/MdProGithub/bootstrap1" className="btn btn-primary">Voir</a>
+                  <a href="https://github.com/MdProGithub/bootstrap1" target='_blank' className="btn btn-primary">Voir</a>
                 </div>
               </div>
 
@@ -20,7 +20,7 @@ const Achievement = () => {
                 <div className="card-body text-center">
                   <h5 className="card-title">Maquettage</h5>
                   <p className="card-text">Prototype de site web conçu avec Figma, offrant une prévisualisation interactive des fonctionnalités et du design avant le développement.</p>
-                  <a href="https://github.com/MdProGithub/Probeats_FIGMA" className="btn btn-primary">Voir</a>
+                  <a href="https://github.com/MdProGithub/Probeats_FIGMA"target='_blank' className="btn btn-primary">Voir</a>
                 </div>
               </div>
 
@@ -29,7 +29,7 @@ const Achievement = () => {
                 <div className="card-body text-center">
                   <h5 className="card-title">Fonctionnalité JS</h5>
                   <p className="card-text">Site web moderne avec un système de commentaires dynamique, conçu pour une interaction utilisateur simple et efficace.</p>
-                  <a href="https://github.com/MdProGithub/formulaire" className="btn btn-primary">Voir</a>
+                  <a href="https://github.com/MdProGithub/formulaire" target='_blank' className="btn btn-primary">Voir</a>
                 </div>
               </div>
             </div>
